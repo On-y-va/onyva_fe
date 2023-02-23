48036bbc82d212708625d6f44dd503e9bec9b415
Braxton Eddings <beddings81@gmail.com>
2023-02-23 11:25:24 -0600

    Complete user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :new]
 
   resources :trips, only: [:new]
+
+  post '/users/new', to: 'users#create'
 end
--- spec/features/welcome/index_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'welcome page' do
-  it 'contains a header' do
-    visit root_path
-
-    expect(page).to have_content("Welcome to ONYVA")
-  end
-end
