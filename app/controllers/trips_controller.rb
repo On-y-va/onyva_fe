20c8bdc95cb8cdbbe12434f69c48813438fc1af3
Alastair Lloyd <dex8056@aol.com>
2023-02-23 16:38:32 -0700

    add functionality to create a new trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -7,4 +7,19 @@ class TripsController < ApplicationController
 
   def new
   end
+
+  def create
+    trip_params = ({
+      "name": params[:name],
+      "country": params[:country],
+      "city": params[:city],
+      "postcode": params[:postcode],
+      "place_id": params[:place_id]
+      })
+      
+      conn = Faraday.new
+      response = conn.post("http://localhost:5000/api/v1/trips", trip: trip_params)
+      trip = JSON.parse(response.body, symbolize_names: true)[:data]
+      redirect_to trip_path(trip[:id].to_i)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :trips, only: [:show, :new]
 
   post '/users/new', to: 'users#create'
+  post '/trips/new', to: 'trips#create'
   get '/login', to: "users#login_form"
   post '/login', to: 'users#login_user'
 end
