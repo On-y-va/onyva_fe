6d5dcbf7e8759145d2b1f11d0da53b1a34568e5f
Alastair Lloyd <dex8056@aol.com>
2023-02-23 10:13:37 -0700

    update welcome with active buttons and styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,23 @@ class UsersController < ApplicationController
   end
 
   def edit
-    
+  end
+
+  def login_form
+  end
+  
+  def new
+  end
+
+  def login_user
+    user = User.find_by(email: params[:email])
+    if user.authenticate(params[:password])
+      # session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.first_name}"
+      redirect_to user_path(user)
+    else
+      flash[:error] = "Sorry, your credentials are bad."
+      render :login_form
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :new]
 
   resources :trips, only: [:new]
+
+  get '/login', to: "users#login_form"
+  post '/login', to: 'users#login_user'
 end
