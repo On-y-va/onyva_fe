83ca77146ed436ed792ff137abd3fc9852036a47
Braxton Eddings <beddings81@gmail.com>
2023-02-22 18:45:15 -0600

    Update user poro

--- app/poros/user.rb
+++ app/poros/user.rb
@@ -6,8 +6,8 @@ class User
     @first_name = data[:first_name]
     @last_name = data[:last_name]
     @phone_number = data[:phone_number]
-    @email= data[:email]
-    @password = data[:password]
+    @email = data[:email]
+    # @password = data[:password]
     @emergency_contact_name = data[:emergency_contact_name]
     @emergency_contact_phone_number = data[:emergency_contact_phone_number]
   end
--- spec/poros/user_spec.rb
+++ spec/poros/user_spec.rb
@@ -1,7 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  xit 'exist and has attributes' do
-    
+  it 'exist and has attributes' do
+    data = {
+            email: "b@email.com",
+            emergency_contact_name: nil,
+            emergency_contact_phone_number: nil,
+            first_name: "Braxton",
+            id: "1",
+            last_name: "Eddings",
+            phone_number: "1234567"
+          }
+
+    user = User.new(data)
+
+    expect(user).to be_a(User)
+    expect(user.id).to eq("1")
+    expect(user.first_name).to eq("Braxton")
+    expect(user.last_name).to eq("Eddings")
+    expect(user.phone_number).to eq("1234567")
+    expect(user.email).to eq("b@email.com")
+    expect(user.emergency_contact_name).to eq(nil)
+    expect(user.emergency_contact_phone_number).to eq(nil)
   end
 end
