532e1da7cd089d09c63d59347ca2c84b2c198344
Braxton Eddings <beddings81@gmail.com>
2023-02-23 23:36:06 -0600

    Update sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,9 @@ class SessionsController < ApplicationController
     auth_hash = request.env['omniauth.auth']
     user_email = auth_hash[:info][:email] 
     conn = Faraday.new
-    response = conn.get("http://localhost:5000/api/v1/users", email: user_email)
-    session[:user_id] = response[:user][:id]
+    response = conn.get("http://localhost:5000/api/v1/users/find", email: user_email)
+    user = JSON.parse(response.body, symbolize_names: true)[:data]
+    session[:user_id] = user[:id]
     redirect_to user_path(session[:user_id])
   end
 end
