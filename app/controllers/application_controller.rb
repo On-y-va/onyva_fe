8c30ec9d7406ddc3f703bfc1eb0ca05f793c6f51
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-02-26 09:23:43 -0800

    Refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,21 +4,8 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    if @current_user == nil
-      if session[:user_email]
-        # conn = Faraday.new
-        # response = conn.get("https://onyva-be.herokuapp.com/api/v1/users/find", email: user_email)
-        # @current_user = JSON.parse(response.body, symbolize_names: true)[:data]
-        @current_user = UserFacade.find_user_by_email(session[:user_email])
-        session[:user_email] = nil
-        session[:user_id]= @current_user.id
-        # require 'pry'; binding.pry
-        # User.find(session[:user_id])
-      else
-        @current_user
-      end
-    end
-
-    # @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= UserFacade.find_user_by_email(session[:user_email]) if session[:user_email]
+    session[:user_email] = nil
+    session[:user_id]= @current_user.id
   end
 end
