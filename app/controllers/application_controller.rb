47db803b6840b5fb6adb1a729dcc8b8d725517e0
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-02-26 19:42:43 -0800

    Reformat

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    require 'pry'; binding.pry
     @current_user ||= UserFacade.find_by_uid(session[:uid]) if session[:uid]
-    # @current_user ||= UserFacade.find_user_by_email(session[:user_email]) if session[:user_email]
-    # @current_user ||= Userfacade.one_user(params[:id]) 
-    # session[:user_email] = nil
-    # session[:user_id]= @current_user.id if @current_user
-    # @current_user
   end
 end
