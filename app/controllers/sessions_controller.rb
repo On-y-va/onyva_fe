41c50abd607e5739283622170064bc0119b6ad59
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-02-26 18:04:53 -0800

    Update sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,30 @@
 class SessionsController < ApplicationController
   def create
-    auth_hash = request.env['omniauth.auth']
-    session[:user_email] = auth_hash[:info][:email] 
+    # session[:auth_hash] = request.env['omniauth.auth']
+    # require 'pry'; binding.pry
+    auth_hash = request.env['omniauth.auth'] 
+    session[:uid] = auth_hash[:uid]
     if current_user
+      # session[:auth_hash] = nil
+      session[:user_id] = @current_user.id
       redirect_to user_path(session[:user_id])
     else
-      flash[:error] = "Please create an account to continue"
-      redirect_to new_user_path
+      # redirect_to(:controller => "users", :action => "create")
+      # post request to BE to make new user with auth hash data
+      # flash[:error] = "Please create an account to continue"
+      # redirect_to users_new_path
+      user = {
+        first_name: auth_hash[:info][:first_name],
+        last_name: auth_hash[:info][:last_name],
+        email: auth_hash[:info][:unverified_email],
+        uid: auth_hash[:uid]
+      }
+      conn = Faraday.new
+      
+      # response = conn.post("https://onyva-be.herokuapp.com/api/v1/users", user: user_params)
+      response = conn.post("http://localhost:5000/api/v1/users", user: user)
+      user = JSON.parse(response.body, symbolize_names: true)[:data]
+      redirect_to user_path(user[:id])
     end
   end
 
