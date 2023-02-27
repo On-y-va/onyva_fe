71d766cb0b64b7ddcf840f26ce7389158f5172d8
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-02-26 18:03:21 -0800

    Update current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,12 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    @current_user ||= UserFacade.find_user_by_email(session[:user_email]) if session[:user_email]
-    session[:user_email] = nil
-    session[:user_id]= @current_user.id
+    require 'pry'; binding.pry
+    @current_user ||= UserFacade.find_by_uid(session[:uid]) if session[:uid]
+    # @current_user ||= UserFacade.find_user_by_email(session[:user_email]) if session[:user_email]
+    # @current_user ||= Userfacade.one_user(params[:id]) 
+    # session[:user_email] = nil
+    # session[:user_id]= @current_user.id if @current_user
+    # @current_user
   end
 end
