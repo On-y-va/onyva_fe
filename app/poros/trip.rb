b3fddbd5188a0603480a60b2b7b33923902298cb
Braxton Eddings <beddings81@gmail.com>
2023-02-26 20:10:06 -0600

    Add images to trips show page

--- app/poros/trip.rb
+++ app/poros/trip.rb
@@ -1,5 +1,5 @@
 class Trip
-  attr_reader :trip_id, :name, :country, :city, :postcode, :place_id, :start_time, :end_time, :events
+  attr_reader :trip_id, :name, :country, :city, :postcode, :place_id, :start_time, :end_time, :events, :image_url
 
   def initialize(info)
     @trip_id = info[:data][:id]
@@ -10,6 +10,7 @@ class Trip
     @place_id = info[:data][:attributes][:place_id]
     @start_time = info[:data][:attributes][:start_time]
     @end_time = info[:data][:attributes][:end_time]
+    @image_url = info[:data][:attributes][:image_url]
     @events = info[:included]
   end
 end
