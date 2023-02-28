fddb951ca8a2887e5dbd9e9ae555d93306887381
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-02-28 07:20:53 -0800

    Update: remove routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  resources :users, except: [:index]
+  resources :users, except: [:index, :new, :create]
   resources :trips do
     resources :flights, only: [:index, :new]
   end
-
   post '/trips/:trip_id/flights/new', to: 'flights#create'
-  post '/trips/new', to: 'trips#create'
+
   get '/auth/:provider/callback', to: 'sessions#create'
   get "/logout" => "sessions#destroy", :as => :logout
 end
