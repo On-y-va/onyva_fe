dd048151087d3a134ef08d2880c6c99bec5e5b24
Alastair Lloyd <dex8056@aol.com>
2023-02-28 17:12:52 -0700

    deleted event as relized this is now handled differently

--- app/facades/event_facade.rb
+++
@@ -1,8 +0,0 @@
-class EventFacade
-  def self.events(id)
-    results = OnyvaService.events(id)
-    results.map do |event|
-       Event.new(event)
-    end
-  end
-end
--- app/poros/event.rb
+++
@@ -1,8 +0,0 @@
-class Event
-  attr_reader :name, :address
-
-  def initialize(data)
-    @name = data[:attributes][:name]
-    @address = data[:attributes][:address]
-  end
-end
