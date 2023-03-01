67a70282a847e77d253e851b4a1b8bab93f838b8
Alastair Lloyd <dex8056@aol.com>
2023-03-01 13:40:13 -0700

    tests updated and passing, My trips button added

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -2,30 +2,38 @@ require 'rails_helper'
 
 RSpec.describe 'user show page', :vcr do
   before :each do
-    visit user_path(1)
+    visit "/auth/google_oauth2"
+
+    Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
   end
 
   it 'displays the users name' do
-    expect(page).to have_content('braxton Eddings')
+    visit user_path(1)
+    expect(page).to have_content('John Doe')
   end
 
   it 'has a button to create a trip' do
-    click_button 'Create Trip'
-
-    expect(current_path).to eq(new_trip_path) 
+    visit user_path(1)
+    expect(page).to have_button('Create Trip')
   end
 
   it 'has a button to edit profile' do
-    click_button 'Edit Profile'
+    visit user_path(1)
+    expect(page).to have_button('Edit Profile')
+  end
 
-    expect(current_path).to eq(edit_user_path(1))
+  it 'has a button to go to my trips' do
+    visit user_path(1)
+    expect(page).to have_button('My Trips')
   end
 
   it 'has a section for upcoming trips' do
+    visit user_path(1)
     expect(page).to have_content('Upcoming Trips')
   end
 
   it 'has a section for pending trips' do
+    visit user_path(1)
     expect(page).to have_content('Pending Trips')
   end
 end
