4853ec2209ee26ae42b3cc80e513c43b9d8b9652
Braxton Eddings <beddings81@gmail.com>
2023-02-28 23:52:28 -0600

    Complete user accept/decline trip function with sad path testing upon trip creation

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
     resources :flights, only: [:index, :new]
   end
   post '/trips/:trip_id/flights/new', to: 'flights#create'
-
+  get "/api/v1/users/:user_id/trips/:trip_id", to: 'trip_attendees#update'
+  get "/api/v1/users/:user_id/trips/:trip_id/delete", to: 'trip_attendees#destroy'
   get '/auth/:provider/callback', to: 'sessions#create'
   get "/logout" => "sessions#destroy", :as => :logout
 end
