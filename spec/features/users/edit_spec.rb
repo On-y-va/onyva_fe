25c63a0f23d8953afe968dc128adbacf379edde1
Braxton Eddings <beddings81@gmail.com>
2023-02-28 18:38:07 -0600

    Update users show

--- spec/features/users/edit_spec.rb
+++ spec/features/users/edit_spec.rb
@@ -1,23 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe 'users edit page', :vcr do
-  it 'has a form to edit user' do
-    visit edit_user_path(1)
+  before :each do
+    visit "/auth/google_oauth2"
 
+    Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+  end
+  
+  it 'has a form to edit user' do
+    visit edit_user_path(2)
+
+    expect(page).to have_field(:first_name)
+    expect(page).to have_field(:last_name)
     expect(page).to have_field(:phone_number)
     expect(page).to have_field(:emergency_contact_name)
     expect(page).to have_field(:emergency_contact_phone_number)
     expect(page).to have_button("Update")
   end
 
-  xit 'redirects to the user show page upon successful update' do
-    visit edit_user_path(1)
+  it 'redirects to the user show page upon successful update' do
+    visit edit_user_path(2)
 
-    fill_in(:phone_number, with: "111111111111")
+    fill_in(:first_name, with: "Ashley")
     
     click_button("Update")
 
-    expect(current_path).to eq(user_path(1))
-    # expect(@user.1).to have(:phone_number '111111111111')
+    #this within block is on user show page
+    within("#user_name") do
+      expect(page).to have_content("Ashley")
+    end
   end
 end
