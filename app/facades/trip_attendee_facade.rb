587338c3457c119f70112a603e0563d88f1f729e
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-03-02 09:16:10 -0800

    Debug trip attendee delete

--- app/facades/trip_attendee_facade.rb
+++ app/facades/trip_attendee_facade.rb
@@ -4,6 +4,6 @@ class TripAttendeeFacade
   end
     
   def self.delete_trip_attendee(user_id, trip_id)
-    OnyvaService.update_trip_attendee(user_id, trip_id)
+    OnyvaService.delete_trip_attendee(user_id, trip_id)
   end
 end
