bfa8335500506d2580ab3d598d643e83294718f6
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-03-02 09:17:47 -0800

    Remove pry

--- spec/facade/flight_facade_spec.rb
+++ spec/facade/flight_facade_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe FlightFacade do
   it 'returns a flights poro', :vcr do
    flights = FlightFacade.flights(1)
-require 'pry'; binding.pry
+   
    expect(flights).to be_a(Array)
    expect(flights[0]).to be_a(Flight)
    expect(flights[1]).to be_a(Flight)
