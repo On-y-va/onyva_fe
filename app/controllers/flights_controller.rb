c079c9263d3d39f0be105351e0f52de0c3069a26
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-03-01 18:22:40 -0800

    Refactor flights controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -8,23 +8,19 @@ class FlightsController < ApplicationController
   end
 
   def create
-    flight_params = ({
+      conn = Faraday.new
+      response = conn.post("https://onyva-be.herokuapp.com/api/v1/trips/#{params[:trip_id]}/flights", flight: flight_params)
+      # response = conn.post("http://localhost:5000/api/v1/trips/#{trip_id}/flights", flight: flight_params)
+      flight = JSON.parse(response.body, symbolize_names: true)
+      redirect_to trip_path(params[:trip_id])
+  end
+
+  def flight_params
+    {
       "email": params[:email],
       "date": params[:date],
       "airline_code": params[:airline_code],
       "flight_number": params[:flight_number]
-      })
-      trip_id = params[:trip_id]
-      conn = Faraday.new
-      response = conn.post("https://onyva-be.herokuapp.com/api/v1/trips/#{trip_id}/flights", flight: flight_params)
-      # response = conn.post("http://localhost:5000/api/v1/trips/#{trip_id}/flights", flight: flight_params)
-      flight = JSON.parse(response.body, symbolize_names: true)
-      redirect_to trip_path(trip_id)
+      } 
   end
-
-  # def destroy
-  #   trip_id = params[:id]
-  #   redirect_to root_path
-  #   flash[:notice] = "Trip was successfully deleted."
-  # end
 end
