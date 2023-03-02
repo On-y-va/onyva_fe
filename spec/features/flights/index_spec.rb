14cf92ef4a6e81ad9d50420bee856f93f6a448fc
Alastair Lloyd <dex8056@aol.com>
2023-03-02 14:16:40 -0700

    flights index vcr with seed

--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -2,50 +2,69 @@ require 'rails_helper'
 
 RSpec.describe 'flight index page', :vcr do
   before :each do
-    visit trip_flights_path(3)
+    visit "/auth/google_oauth2"
+
+    Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
   end
 
   it 'has the onyva logo' do
+    click_on "Trippin"
+    click_button "Flight Information"
+
     within ('#logo') do
       expect(page).to have_css("img[src*='https://raw.githubusercontent.com/On-y-va/onyva_fe/main/src/assets/onyva_logo.png']")
     end
   end
 
   it 'contains a header Flight Information' do
-    expect(page).to have_content("Flight Information")
+    click_on "Trippin"
+    click_button "Flight Information"
+
+    expect(page).to have_content("Group Flight Information")
   end
 
   it 'has a button to return to the trip show page' do
+    click_on "Trippin"
+    click_button "Flight Information"
+
     within("#button") do
       expect(page).to have_button 'Trip Details'
     end
   end
 
-  it 'has a button to return to the trip show page' do
-    click_button 'Trip Details'
-  end
-
   it 'has user email info' do
+    click_on "Trippin"
+    click_button "Flight Information"
+
     within("#table") do
-      expect(page).to have_content('email') #make dynamic
+      expect(page).to have_content('User Email')
     end
   end
 
   it 'has flight Airline code info' do
+    click_on "Trippin"
+    click_button "Flight Information"
+
     within("#table") do
-      expect(page).to have_content('Airline Code') #make dynamic
+      expect(page).to have_content('Airline Code')
     end
   end
 
   it 'has flight Flight number info' do
+    click_on "Trippin"
+    click_button "Flight Information"
+
     within("#table") do
-      expect(page).to have_content('Flight Number') #make dynamic
+      expect(page).to have_content('Flight Number')
     end
   end
 
   it 'has flights flight date info' do
+    click_on "Trippin"
+    click_button "Flight Information"
+
     within("#table") do
-      expect(page).to have_content('2023-03-01T00:00:00.000Z') #make dynamic
+      expect(page).to have_content('Date')
     end
   end
 end
